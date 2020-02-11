b6724b9583d87ddf74013790d33d8e56bfb1f928
sj123 <1542819432@qq.com>
2020-02-11 16:05:00 +0800

    创建项目 创建应用

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+# 每一张表就是一个模型类
+
+class Book(models.Model):
+    """
+    book继承了Model类 应为Model类拥有操作数据库的功能
+    """
+    title = models.CharField(max_length=20)
+    price = models.FloatField(default=0)
+    pub_date = models.DateField(default="1983-06-01")
+
+
+class Hero(models.Model):
+    """
+    hero继承了model 也可以操作数据库
+    """
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), default='male')
+    content = models.CharField(max_length=100)
+    # book 是一对多的外键 on_delete代表删除主表数据时如何做
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
