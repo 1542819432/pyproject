030f15346d448093fa93af0d2c1f539179d395b2
sj123 <1542819432@qq.com>
2020-02-14 18:04:22 +0800

    11

--- end/demo1/bookdemo/booktest/urls.py
+++ end/demo1/bookdemo/booktest/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     # 第一个参数就是路由 第二个参数就是视图函数
     # 第一个参数中如果有正则分组 小括号 则正则分组匹配的内容会作为实参传递
     url(r'detail/(\d+)/',views.detail,name='detail'),
-    url(r'^about/$',views.about,name='about')
+    url(r'^about/$',views.about,name='about'),
+
+    url(r'^deletebook/(\d+)/$',views.deletebook,name='deletebook'),
+    url(r'^deletehero/(\d+)/$',views.deletehero,name='deletehero'),
+    url(r'^addhero/(\d+)/$',views.addhero,name='addhero'),
+    url(r'^edithero/(\d+)/$',views.edithero,name='edithero'),
+
 ]
--- end/demo1/bookdemo/booktest/views.py
+++ end/demo1/bookdemo/booktest/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.template import loader
-from .models import Book,Hero
+from .models import Book, Hero
 
 # Create your views here.
 
@@ -8,8 +8,10 @@ from .models import Book,Hero
 
 
 # 3编写对应的视图函数
-from django.http import HttpResponse
-def index(resquest):
+from django.http import HttpResponse, HttpResponseRedirect
+
+
+def index(request):
     # # return HttpResponse("这里是首页")
     # # 1获取模板
     # template = loader.get_template('index.html')
@@ -21,10 +23,10 @@ def index(resquest):
     # return HttpResponse(result)
 
     # 3合一
-    return render(resquest,'index.html',{"books":books})
+    return render(request, 'index.html', {"books": books})
 
 
-def detail(resquest,bookid):
+def detail(request, bookid):
     # # return HttpResponse("这里是详情页"+bookid)
     # template = loader.get_template('detail.html')
     book = Book.objects.get(id=bookid)
@@ -32,10 +34,56 @@ def detail(resquest,bookid):
     # result = template.render(constext)
     # return HttpResponse(result)
 
-    return render(resquest,'detail.html',{"book":book})
+    return render(request, 'detail.html', {"book": book})
 
-def about(resquest):
-    return  HttpResponse("这里是关于")
+
+def deletebook(request, bookid):
+    book = Book.objects.get(id=bookid)
+    book.delete()
+    # return HttpResponse("删除成功")
+    # return HttpResponseRedirect(redirect_to='/')
+    return redirect(to='/')
+
+
+def deletehero(request, heroid):
+    hero = Hero.objects.get(id=heroid)
+    bookid = hero.book.id
+    hero.delete()
+
+    url = reverse("booktest:detail", args=(bookid,))
+    return redirect(to=url)
+
+
+def addhero(request,bookid):
+    if request.method == "GET":
+        return render(request,'addhero.html')
+    elif request.method == "POST":
+        hero = Hero()
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("sex")
+        hero.book = Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse("booktest:addhero",args=(bookid,))
+        return redirect(to=url)
+
+def edithero(request,heroid):
+    hero = Hero.objects.get(id=heroid)
+
+    if request.method == "GET":
+        return render(request,'edithero.html',{"hero":hero})
+    elif request.method == "POST":
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("sex")
+        hero.save()
+        url = reverse("booktest:detail",args=(hero.book.id,))
+        return redirect(to=url)
+
+
+
+def about(request):
+    return HttpResponse("这里是关于")
 
 # 使用django 模板
 # MVT
